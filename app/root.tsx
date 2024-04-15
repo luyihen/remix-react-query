98163247950aed04e8dc499bfce561f2cd18d217
henry <henry.lu@riversoft.com.tw>
2024-04-15 16:55:10 +0800

    init

--- app/root.tsx
+++ app/root.tsx
@@ -1,29 +1,33 @@
 import {
-  Links,
+  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+        </head>
+        <body>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </body>
+      </html>
+    </QueryClientProvider>
+  );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,60 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, LoaderFunction } from "@remix-run/node";
+import { useQuery } from "@tanstack/react-query";
+import { useLoaderData, useSearchParams } from "@remix-run/react";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const idx = url.searchParams.get("idx") || "1";
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${idx}`);
+  const data = await response.json();
+  return json(data);
+};
+
+const fetchPokeAPI = async (idx: number) => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${idx}/`);
+  const data = await res.json();
+  return data;
 };
 
 export default function Index() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const idx = parseInt(searchParams.get("idx") || "1", 10);
+
+  console.log("useLoaderData: ", useLoaderData())
+
+  const { data, isFetching } = useQuery({
+    queryKey: ["PokeAPI", idx],
+    queryFn: () => fetchPokeAPI(idx),
+    initialData: useLoaderData(),
+    staleTime: 10000,
+    gcTime: 20000,
+  });
+
+  if (isFetching) {
+    return <span>Loading...</span>;
+  }
+
+  const setPrev = () => {
+    if (idx > 1) setSearchParams({ idx: String(idx - 1) });
+  };
+
+  const setNext = () => {
+    setSearchParams({ idx: String(idx + 1) });
+  };
+
+  console.log(data);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <div>
+        <img src={data?.sprites?.front_default} alt="Pokemon" />
+      </div>
+      <div>
+        <button onClick={setPrev}>上一個</button>
+        <button onClick={setNext}>下一個</button>
+      </div>
+      <div>
+        <p>current id: {data?.id}</p>
+      </div>
     </div>
   );
 }
